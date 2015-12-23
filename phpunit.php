5e636079293cc44a4b875541244fe2c026814fcc
Severin Kaderli <severin.kaderli@gmail.com>
2015-12-23 19:21:25 +0100

    Added all important constants to phpunit.php

--- phpunit.php
+++ phpunit.php
@@ -1,8 +1,14 @@
 <?php
 
-//Here I set some PHP $_SERVER variables that are not set in cli mode but
-//that I use in my code.
+//Declaring some constants and $_SERVER variables that are needed for the 
+//framework but are set by the cli-php by default.
 $_SERVER["SERVER_NAME"] = "example.com";
 
-define("BASE_PATH", "/subdir/kuro");
+define("__ROOT__", __DIR__ . "/");
+define("PROTOCOL", "http://");
+define("BASE_URL", PROTOCOL . $_SERVER["SERVER_NAME"] . "/subdir/kuro");
+define("SERVER_URL", PROTOCOL . $_SERVER['SERVER_NAME']);
+define("BASE_PATH", str_replace(SERVER_URL, "", BASE_URL));
+
+//Include the composer autoload file
 require_once("./vendor/autoload.php");
